889a5845e007064c73da2f94c4aa147285e61ca0
Gustavo <ofoxsmith@outlook.com>
2023-09-04 21:33:46 +1000

    remove erekir and serpulo items

--- src/copious/content/planets/CPlanets.java
+++ src/copious/content/planets/CPlanets.java
@@ -1,5 +1,6 @@
 package copious.content.planets;
 import arc.graphics.Color;
+import mindustry.content.Items;
 import mindustry.content.Planets;
 import mindustry.graphics.g3d.HexMesh;
 import mindustry.type.Planet;
@@ -9,16 +10,17 @@ public class CPlanets {
     public static Planet tantros;
 
     public static void load() {
-        tantros = new Planet("tantros", Planets.sun, 1f, 2){{
+        tantros = new Planet("tantros", Planets.sun, 1.5f, 2){{
             generator = new CTantrosPlanetGenerator();
             meshLoader = () -> new HexMesh(this, 4);
             alwaysUnlocked = true;
-            atmosphereColor = Color.valueOf("3db899");
+            atmosphereColor = Color.blue;
             iconColor = Color.valueOf("597be3");
             startSector = 10;
-            atmosphereRadIn = -0.01f;
-            atmosphereRadOut = 0.3f;
+            atmosphereRadIn = -0.5f;
+            atmosphereRadOut = 0.5f;
             defaultEnv = Env.underwater | Env.terrestrial;
+            hiddenItems.addAll(Items.serpuloItems).addAll(Items.erekirItems);
             ruleSetter = r -> {
 
             };
--- src/copious/content/planets/CTantrosPlanetGenerator.java
+++ src/copious/content/planets/CTantrosPlanetGenerator.java
@@ -2,7 +2,8 @@ package copious.content.planets;
 import arc.math.geom.Vec3;
 import mindustry.content.Blocks;
 import mindustry.maps.generators.PlanetGenerator;
+import mindustry.maps.planet.TantrosPlanetGenerator;
 import mindustry.world.TileGen;
 
-public class CTantrosPlanetGenerator extends PlanetGenerator {
+public class CTantrosPlanetGenerator extends TantrosPlanetGenerator {
 }
