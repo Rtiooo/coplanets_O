1ff181d58dff0e0c74ab87232ffdc220fe2a01dc
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2023-09-11 09:37:16 +1000

    add items file

---
+++ src/copious/content/CItems.java
@@ -0,0 +1,7 @@
+package copious.content;
+import mindustry.type.Item;
+
+public class CItems {
+    // titanium, sand, thorium and silicon are already in the game
+    public static Item iron, nickel, quartz, salt, aluminium, glass;
+}
