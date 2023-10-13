e79d34b0cb21d10079668297a53c8db4dc763898
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2023-10-13 14:19:30 +1100

    add first core and empty sprites

--- src/copious/content/CBlocks.java
+++ src/copious/content/CBlocks.java
@@ -1,10 +1,17 @@
 package copious.content;
-import mindustry.world.Block;
+import mindustry.type.Category;
+import mindustry.content.UnitTypes;
+import mindustry.world.*;
 import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.storage.*;
+import static mindustry.type.ItemStack.*;
 public class CBlocks {
     public static Block
     //env
-    testFloor, waterOverlay;
+    testFloor, waterOverlay,
+
+    //cores
+    corePhotic, pelagic, hadal;
 
     public static void load() {
         testFloor = new Floor("testFloor"){{
@@ -13,6 +20,21 @@ public class CBlocks {
 
         waterOverlay = new OverlayFloor("waterOverlay"){{
             variants = 1;
+        }};
+
+        corePhotic = new CoreBlock("core-photic"){{
+            requirements(Category.effect, with(CItems.iron, 1500, CItems.silicon, 500, CItems.nickel, 1000));
+
+            isFirstTier = true;
+            unitType = UnitTypes.alpha;
+            health = 3000;
+            armor = 5f;
+            itemCapacity = 2000;
+            incinerateNonBuildable = true;
+            size = 3;
+            alwaysUnlocked = true;
+            buildCostMultiplier = 0.5f;
+            unitCapModifier = 8;
         }};
     }
 }
